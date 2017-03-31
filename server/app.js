74d103a11b2cee66b35f0fc9b328c3eca7e72200
Lisa Schoofs <lisaschoofs@gmail.com>
2017-03-31 15:30:01 -0500

    base node server and file structure

---
+++ server/app.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//Database Variables
+var mongoose = require("mongoose");
+// var mongoURI = "mongodb://localhost:27017/company";
+// var MongoDB = mongoose.connect(mongoURI).connection;
+
+
+// MongoDB.on("error", function(err){
+//   console.log("Mongo Connection Error :" + err);
+// });
+//
+
+// MongoDB.once("open", function(){
+//   console.log("Tots connected to Mongo, meow.");
+// });
+
+//Set the port
+app.set("port", (process.env.PORT || 5000));
+
+//Routes
+//base get
+app.get("/", function(req,res){
+  res.sendFile(path.resolve("server/public/views/index.html"));
+});
+
+//Middleware hookups
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("./server/public/"));
+
+//Listen
+app.listen(app.get("port"), function(){
+    console.log("Listening on port: " + app.get("port"));
+});
