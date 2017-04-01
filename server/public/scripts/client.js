eb4855ebdf6a35f2675f2fd303388cd2c2601a1f
Lisa Schoofs <lisaschoofs@gmail.com>
2017-04-01 11:12:44 -0500

    start of POST route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   console.log("jQuery ready to rock");
   getListings();
+  newRental();
 });
 
 function getListings() {
@@ -12,22 +13,57 @@ function getListings() {
       appendListings(response);
     }//end success
   });//end ajax
+}//ends getListings
+
+function newRental() {
+  $.ajax({
+    type: "POST",
+    url: "/listings",
+    data: {city: "test4",
+           sqft: 1004,
+           rent: 1004
+          },
+    success: function(response){
+      // appendListings();
+      console.log(response);
+    }
+  });
 }
 
 function appendListings(listings) {
     for (var i = 0; i < listings.length; i++) {
         var listing = listings[i];
-        console.log(listing);
+        // console.log(listing);
         $("#listings").append("<div class='well col-md-3'></div>");
         var $el = $("#listings").children().last();
         $el.append("<p>" + listing.city + "</p>");
         $el.append("<p>" + listing.sqft + "</p>");
     if (listing.cost) {
         $el.append("<p> Available for Purchase!<p>" +
-                   "<p> Cost: " + listing.cost + "</p>");
+                   "<p> Cost: $" + listing.cost + "</p>");
     } else {
         $el.append("<p> Available for Rent!<p>" +
-                   "<p> Monthly Rent : " + listing.rent + "</p>");
-    }
-}
+                   "<p> Monthly Rent : $" + listing.rent + "</p>");
+    } //ends else
+  } //ends for loop
+} //ends appendListings
+
+// function addListing() {
+//   $.ajax({
+//     type: "POST",
+//     url: "/listings/:" + id,
+//     data: {city: "", sqft: "", cost: "", rent: ""},
+//     success: function(response){
+//       // appendListings();
+//       console.log(response);
+//     }
+//   });
+// }
+//
+// }
+
+
+
+function newHouse() {
+
 }
--- server/routes/listings.js
+++ server/routes/listings.js
@@ -2,16 +2,14 @@ var express = require("express");
 var router = express.Router();
 var mongoose = require("mongoose");
 
-
 var RentSchema = mongoose.Schema({
-  cost: Number,
-  // rent: Number,
+  rent: Number,
   sqft: Number,
   city: String
 });
 
 var BuySchema = mongoose.Schema({
-  rent: Number,
+  cost: Number,
   sqft: Number,
   city: String
 });
@@ -27,9 +25,31 @@ router.get("/", function(req,res){
       console.log(err);
       res.sendStatus(500);
     }
-    console.log(allListings);
+    // console.log(allListings);
     res.send(allListings);
   });
 });
+
+//POST new listing to Database
+router.post("/", function(req,res){
+
+  console.log(req.params);
+  console.log(req.body);
+  var listing = new Rent();
+
+  listing.city = req.body.city;
+  listing.sqft = req.body.sqft;
+  listing.rent = req.body.rent;
+
+  listing.save(function(err, savedListing){
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+console.log(savedListing);
+    res.send(savedListing);
+  });
+});
+
 
 module.exports = router;
